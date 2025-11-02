5baee28fc15ff7219f33336ea2fce0e878dd27a5
Katharina <katharina.cazacova@gmail.com>
2025-11-02 19:09:24 +0100

    Oskar

--- Project1/src/Main.java
+++ Project1/src/Main.java
@@ -1,5 +1,26 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    System.out.println("Oskar");
+
+    String name;
+    name = "Oskar";
+
+    int laenge;
+    laenge = 70;
+
+    double gewicht;
+    gewicht = 7.8;
+
+    boolean schlaf
+            = false;
+
+
+    System.out.println("Meine Katze heißt " + name +
+    " und er ist " + laenge +" cm lang."+"\nEr wiegt "+ gewicht + " Kilogramm.");
+    if(schlaf){
+        System.out.println("Er schläft gerade.");
+    }
+    else{
+        System.out.println("Er isst gerade.");
+    }
 }
